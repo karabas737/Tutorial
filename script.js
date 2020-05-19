aff52a642238cb6564c02d8eccf92538e7855ddd
Sergii <vlasov737@gmail.com>
2020-05-19 21:46:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*Convert Celsius to Fahrenheit - задача на FreeCodeCamp 
+(Javascript Algorithms => Basic Algorithm Scripting)
+(0 °C × 9/5) + 32 = 32 °F
+*/
+var Celsius= prompt ("Please enter what is the temperature in Celsius")
+var Fahrenheit= (Celsius * 1.8 + 32)
+alert (Fahrenheit)
